2534bee4546a9706b853e96b3304d97bc870ac73
Kschmitt11 <kenley.schmitt11@gmail.com>
2025-09-18 13:35:42 -0500

    Base framework for paddle class and screen options

---
+++ main.py
@@ -0,0 +1,29 @@
+from turtle import Turtle, Screen
+from paddle import Paddle
+from ball import Ball
+
+screen = Screen()
+screen.setup(width=800, height=600)
+screen.bgcolor("black")
+screen.title("Pong")
+screen.tracer(0)
+
+r_paddle = Paddle((350, 0))
+l_paddle = Paddle((-350, 0))
+ball = Ball()
+
+screen.listen()
+screen.onkeypress(fun=r_paddle.go_up, key="Up")
+screen.onkeypress(fun=r_paddle.go_down, key="Down")
+screen.onkeypress(fun=l_paddle.go_up, key="w")
+screen.onkeypress(fun=l_paddle.go_down, key="s")
+
+
+game_is_on = True
+while game_is_on:
+
+    screen.update()
+
+
+
+screen.exitonclick()
