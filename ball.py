39340f4b5224c7d50b05bee39a2d7eff1f6133fb
Kschmitt11 <kenley.schmitt11@gmail.com>
2025-09-18 14:18:30 -0500

    Add ball bouncing logic

--- ball.py
+++ ball.py
@@ -6,3 +6,18 @@ class Ball(Turtle):
         self.color("white")
         self.penup()
         self.goto(0,0)
+        self.x_move = 10
+        self.y_move = 10
+
+
+    def move(self):
+        new_x = self.x_move + self.xcor()
+        new_y = self.y_move + self.ycor()
+        self.goto(new_x, new_y)
+
+    def bounce_y(self):
+        self.y_move *= -1
+
+    def bounce_x(self):
+        self.x_move *= -1
+
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from turtle import Turtle, Screen
 from paddle import Paddle
 from ball import Ball
+import time
 
 screen = Screen()
 screen.setup(width=800, height=600)
@@ -20,10 +21,20 @@ screen.onkeypress(fun=l_paddle.go_down, key="s")
 
 
 game_is_on = True
+
 while game_is_on:
+    time.sleep(0.1)
+    ball.move()
 
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce_y()
+
+    if ball.xcor() > 320 and ball.distance(r_paddle) < 50:
+        ball.bounce_x()
+
+    if ball.xcor() < -320 and ball.distance(l_paddle) < 50:
+        ball.bounce_x()
     screen.update()
-
 
 
 screen.exitonclick()
